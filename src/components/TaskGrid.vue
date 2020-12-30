6e9522c2e405c439089a52640ed1ae88fe1286fa
Leandro <lefinochio@hotmail.com>
2020-12-30 11:49:27 -0300

    First commit

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tasks-grid">
+    <template v-if="tasks.length">
+      <Task 
+        v-for="(task, i) in tasks" 
+        :key="task.name" 
+        :task="task" 
+        @taskDeleted="$emit('taskDeleted', i)" 
+        @stateChange="$emit('stateChange', i)"
+      />
+    </template>
+    <p v-else class="no-task">Sua vida está em dia  :)</p>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true }
+  }
+}
+</script>
+
+<style>
+  .tasks-grid {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .tasks-grid .task {
+    margin: 10px;
+  }
+
+  .no-task {
+    color: #aaa;
+    font-size: 1.7rem;
+  }
+</style>
